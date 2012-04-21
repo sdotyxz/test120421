68431f20aae76a7fae720bb5f94d1a33b57ce166
Fuutou Tan <shengdongtan@gmail.com>
2012-04-21 08:34:43 +0800

    Initial Commit

---
+++ test120421/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test120421
+//
+//  Created by Tan Fuutou on 12-04-21.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test120421/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test120421
+//
+//  Created by Tan Fuutou on 12-04-21.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
